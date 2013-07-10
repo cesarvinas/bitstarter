142024dad38eda91f3ca064ce7e369268a433e18
Cesar Vinas <cesar.vinas@live.com>
2013-07-10 02:47:50 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  fs.readFileSync('/bitstarter/index.html', function (err, data) {
+    if (err) throw err;
+    var buffer = new Buffer(data);
+    response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
