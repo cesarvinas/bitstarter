d6413f782fd99558e944c50d91a37c56b4e2c696
Cesar Vinas <cesar.vinas@live.com>
2013-07-10 03:03:24 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  fs.readFileSync('index.html', function (err, data) {
+  fs.readFileSync('index.html', {encoding: 'utf-8'}, function (err, data) {
     if (err) throw err;
     var buffer = new Buffer(data, 'utf-8');
     response.send(buffer.toString('utf-8'));
