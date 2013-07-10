a501d76148d372e8ed164c566bbd48eb3f5f2be0
Cesar Vinas <cesar.vinas@live.com>
2013-07-10 03:32:58 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -4,10 +4,12 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var data = fs.readFileSync('index.html', {encoding: 'utf8'})
+  //var data = fs.readFileSync('index.html', {encoding: 'utf8'})
+  var data = fs.readFileSync('index.html');
   //var buffer = new Buffer(data, 'utf-8');
   //response.send(buffer.toString('utf-8'));
-  response.send(data);
+  //response.send(data);
+  response.send(data.toString());
     //response.send('hola');
 });
 
